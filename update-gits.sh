b2da512e7938ccbd7c422adceaf36b613026a8d1
Jozef Mlich <jmlich83@gmail.com>
2025-06-20 09:25:14 +0000

    Update modules

--- update-gits.sh
+++ update-gits.sh
@@ -4,8 +4,10 @@ for i in $(find . -name '.git'); do
     d=${i%.git} 
     cd $d 
     echo $d 
-    git checkout master
     git checkout main
     git pull 
+    if [ "$(git symbolic-ref refs/remotes/origin/HEAD)" != "refs/remotes/origin/main" ]; then
+        echo "ERR: default branch is not main!"
+    fi
     cd - >/dev/null
 done
